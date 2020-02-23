2306fead429965317a466e853a7c05974e2c10f6
Jason Lea-Jones <jleajones@gmail.com>
2020-02-22 20:13:47 -0500

    Fixes issue #1

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Index = () => {
   return (
     <div>
-      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/dashboard">Dashboard Link</Link>
     </div>
   );
 };
