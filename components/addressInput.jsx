0bac5907932ed161d6e86571722474fb1b483e62
Jason Lea-Jones <jleajones@gmail.com>
2020-02-23 10:02:06 -0500

    updates for no internet

--- components/addressInput.jsx
+++ components/addressInput.jsx
@@ -24,12 +24,37 @@ const AddressInput = ({ label = 'Address', id = 'address' }) => {
             const possiblePlaces = result.Response.View[0].Result.map(place =>
               parsePlace(place)
             );
-            console.log('Possible places: ', possiblePlaces);
+            console.log(JSON.stringify(possiblePlaces));
             setPlaces(possiblePlaces);
           }
         },
         error => {
+          // TODO: for no internet
+          const defaultPlaces = [
+            {
+              id: 'NT_.zXhRQ11iG78SyoG3qoHMB_0ID',
+              label: "42 Rue Nor'Wes, Bonne-Esperance, QC G0G, Canada",
+              address: {
+                label: "42 Rue Nor'Wes, Bonne-Esperance, QC G0G, Canada",
+                country: 'CAN',
+                state: 'QC',
+                county: 'Le Golfe-du-St-Laurent',
+                city: 'Bonne-Espérance',
+                street: "Rue Nor'Wes",
+                houseNumber: '42',
+                postalCode: 'G0G',
+                additionalData: [
+                  { value: 'Canada', key: 'CountryName' },
+                  { value: 'Québec', key: 'StateName' },
+                  { value: 'Le Golfe-du-St-Laurent', key: 'CountyName' }
+                ]
+              },
+              displayPosition: [51.4246133, -57.8307947],
+              routePosition: [51.4247401, -57.8308683]
+            }
+          ];
           console.log('Error: ', error);
+          setPlaces(defaultPlaces);
         }
       );
     }
