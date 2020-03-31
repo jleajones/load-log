215d9c97759c5d9f9f77924e9ff48cc3f6241854
Jason Lea-Jones <jason.leajones@vroom.com>
2020-03-31 15:51:07 -0400

    updates for db

--- apollo/db.js
+++ apollo/db.js
@@ -1,14 +1,7 @@
 import knex from 'knex';
+import knexConfig from '../knexfile';
+
+const environment = process.env.ENVIRONMENT || 'development';
 
 // eslint-disable-next-line import/prefer-default-export
-export const db = knex({
-  client: 'pg',
-  connection: {
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    ssl: true
-  }
-  // debug: true
-});
+export const db = knex(knexConfig[environment]);
